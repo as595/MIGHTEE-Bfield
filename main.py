73215a9b36c229a0e292912cbd9964f4c97d5b9a
as595 <ammscaife@gmail.com>
2023-02-22 15:10:19 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+# step 1: read catalogues
+
+# step 2: merge catalogues
+
+# step 3: redshift and galactic latitude filtering
+
+# step 4: get GRMs
+
+# step 5: subtract off GRMs
+
+# step 6: calculate f_excess
+
+# step 7: 
---
+++ utils.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def read_catalogue(catfile):
+
+    
+
+    return df
