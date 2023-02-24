800e41f1fa7a94dc146afb26f74396318fc1329d
Anna <ammscaife@gmail.com>
2023-02-24 16:02:52 +0000

    update merge

--- main.py
+++ main.py
@@ -1,25 +1,208 @@
+import pylab as pl
+from astropy.coordinates import SkyCoord 
+
 from utils import *
 from grm_utils import *
 
+# -----------------------------------------------------------
 # step 1: read catalogues
 xmm12_file = '/Users/ascaife/DATA/MIGHTEE/catalog/XMMLSS/XMMLSS_12/XMMLSS_12_1538856059_1539286252_pol_detections.fits'
-xmm12 = read_catalogue(xmm12_file)
+xmm12c = SkyCoord(34.4625,-4.83306,unit="deg")
+xmm12 = read_catalogue(xmm12_file, xmm12c)
+
 xmm13_file = '/Users/ascaife/DATA/MIGHTEE/catalog/XMMLSS/XMMLSS_13/XMMLSS_13_1538942495_1539372679_pol_detections.fits'
-xmm13 = read_catalogue(xmm13_file)
+xmm13c = SkyCoord(35.175,-4.83306,unit="deg")
+xmm13 = read_catalogue(xmm13_file, xmm13c)
+
 xmm14_file = '/Users/ascaife/DATA/MIGHTEE/catalog/XMMLSS/XMMLSS_14/XMMLSS_14_1539028868_1539460932_pol_detections.fits'
-xmm14 = read_catalogue(xmm14_file)
+xmm14c = SkyCoord(35.84167,-4.83306,unit="deg")
+xmm14 = read_catalogue(xmm14_file, xmm14c)
+
 cosmos_file = '/Users/ascaife/DATA/MIGHTEE/catalog/COSMOS/COSMOS_1587911796_1524147354_1525613583_pol_detections.fits'
-cosmos = read_catalogue(cosmos_file)
+cosmosc = SkyCoord(35.84167,-4.83306,unit="deg") # update this
+cosmos = read_catalogue(cosmos_file, cosmosc)
 
+# -----------------------------------------------------------
 # step 2: merge catalogues
-mightee = merge_cats([xmm12, xmm13, xmm14, cosmos])
+mightee = merge_cats([xmm12, xmm13, xmm14])
+print('# of sources in merged catalogue: {}'.format(mightee.shape[0]))
 
+# -----------------------------------------------------------
+# step 2a: Russ' filter to remove XMMLSS filament:
+polflux_limit = 70.0; frac_p_limit = 30.0
+mightee = mightee[mightee['pol'] > polflux_limit
+mightee = mightee[mightee['P/I'] < frac_p_limit
+print('# of sources in filtered catalogue: {}'.format(mightee.shape[0]))
+stop
+# -----------------------------------------------------------
 # step 3: redshift and galactic latitude filtering
+mightee = filter_z(mightee, has_z=True, spec_z=False)
+print('# of sources (after z filter): {}'.format(mightee.shape[0]))
 
+# redshift histogram:
+pl.rcParams["figure.figsize"] = (8,8)
+pl.subplot(111)
+n, bins, patches = pl.hist(mightee['best_z'], density=False, bins=20, edgecolor='w', facecolor='b', alpha=0.75)
+pl.xlabel('Redshift ($z$)')
+pl.ylabel('Frequency')
+pl.savefig('plots/mightee_z.png')
+pl.show()
+
+# add columns for (l,b)
+mightee['l'], mightee['b'] = j2000_to_gal(mightee['ra'].values, mightee['dec'].values)
+
+# filter b<25:
+mightee = filter_glat(mightee, bmin=25.)
+print('# of sources (after b filter): {}'.format(mightee.shape[0]))
+
+# -----------------------------------------------------------
 # step 4: get GRMs
+ra = np.array(mightee['ra'].values)
+dec= np.array(mightee['dec'].values)
 
+fitsfile = './faradaysky/faraday2020v2.fits'
+grmdata, grmhp = get_grm_data(fitsfile) # from grm_utils.py
+
+# nearest pixel values:
+grm_fs, grm_fs_std = get_grm(ra, dec, grmdata, grmhp) # from grm_utils.py
+mightee['GRM_FS'] = grm_fs
+mightee['GRM_FSerr'] = grm_fs_std
+
+# 1 degree median values:
+grm1_med, grm1_mad, grm1_std = get_grm1(ra, dec, grmdata, grmhp, r=0.5) # 1 degree median values (grm_utils)
+mightee['GRM1'] = grm1_med
+mightee['GRM1err'] = grm1_mad
+
+pl.rcParams["figure.figsize"] = (8,5)
+pl.errorbar(grm_fs, grm1_med, xerr=grm_fs_std, yerr=grm1_mad, ls='', marker='o')
+pl.plot([-10,10],[-10,10], ls=':')
+pl.xlabel('GRM [direct]')
+pl.ylabel('GRM [1 degree median]')
+pl.savefig('./plots/grm.png')
+pl.show()
+
+# -----------------------------------------------------------
 # step 5: subtract off GRMs
+rm      = mightee['RM'].values
+rm_err  = mightee['RM_err'].values
+grm     = mightee['GRM1'].values      # use 1 deg median
+grm_err = mightee['GRM1err'].values   # use 1 deg median
+z       = mightee['best_z'].values
 
+# take difference
+rrm = rm - grm
+
+print("---")
+print("Using GRM1 subtraction: ")
+rrm_rms = np.sqrt(np.std(rrm) - np.std(rm_err) - np.std(grm_err))
+se1 = standard_error_on_stdev(np.std(rrm), len(rrm))
+se2 = standard_error_on_stdev(np.std(rm_err), len(rm_err))
+se3 = standard_error_on_stdev(np.std(grm_err), len(grm_err))
+rrm_rms_ste = np.sqrt(se1**2 + se2**2 + se3**2)
+print("<RRM^2>^0.5 : {:.2f} +/- {:.2f} rad/m^2".format(rrm_rms, rrm_rms_ste))
+
+
+# remove data outside 2 sigma:
+std = np.std(rrm)
+grm_err = grm_err[np.where(np.abs(rrm)<=2*std)]
+rrm = rrm[np.where(np.abs(rrm)<=2*std)]
+print('# of sources (after 2sig rm subtraction): {}'.format(len(rrm)))
+
+rrm_rms_obs = stats.median_abs_deviation(rrm, scale='normal', nan_policy='omit')
+rrm_mad = stats.median_abs_deviation(rrm, nan_policy='omit')
+
+print("---")
+print("Using GRM subtraction: ")
+print("<RRM^2>^0.5 [direct] : {:.2f} rad/m^2".format(np.std(rrm)))
+print("RRM MAD : {:.2f} rad/m^2".format(rrm_mad))
+print("<RRM^2>^0.5 [using MAD] : {:.2f} rad/m^2".format(rrm_rms_obs))
+print("Average GRM error : {:.2f} rad/m^2".format(np.mean(grm_err)))
+
+pl.rcParams["figure.figsize"] = (8,8)
+pl.subplot(111)
+n, bins, patches = pl.hist(rrm, density=False, range=(-25,25), bins=50, edgecolor='w', facecolor='orange', alpha=1.0)
+pl.xlabel('RRM')
+pl.ylabel('Frequency')
+pl.savefig('./plots/mightee_rrms.png')
+pl.show()
+
+# -----------------------------------------------------------
 # step 6: calculate f_excess
+rm      = mightee['RM'].values
+rm_err  = mightee['RM_err'].values
+grm     = mightee['GRM_FS'].values      # use 1 deg median
+grm_err = mightee['GRM_FSerr'].values   # use 1 deg median
+grm1    = mightee['GRM1'].values        # use 1 deg median
+z       = mightee['best_z'].values
 
+eps = 1e-7
+rm_excess = rm  - grm1
+grm_excess= grm - grm1 + eps
+
+f_excess = rm_excess/grm_excess
+
+print("---")
+print("fexcess: {:.2f} +/- {:.2f} ".format(np.median(f_excess), stats.median_abs_deviation(f_excess, nan_policy='omit')))
+print("fexcess stdev: {:.2f}".format(stats.median_abs_deviation(f_excess, scale='normal', nan_policy='omit')))
+
+#pl.rcParams["figure.figsize"] = (8,5)
+#pl.scatter(np.arange(len(f_excess)), f_excess, s=1)
+#pl.xlabel('# source')
+#pl.ylabel('fexcess')
+#pl.ylim(-3,3)
+#pl.show()
+#pl.savefig('./plots/mightee_fexcess.png')
+
+pl.rcParams["figure.figsize"] = (8,8)
+pl.subplot(111)
+n, bins, patches = pl.hist(f_excess, density=False, range=(-50,50), bins=20, edgecolor='w', facecolor='orange', alpha=1.0)
+pl.xlabel(r'$f_{excess}$')
+pl.ylabel('Frequency')
+pl.savefig('./plots/mightee_fexcess.png')
+pl.show()
+
+# -----------------------------------------------------------
 # step 7: 
+rm      = mightee['RM'].values
+rm_err  = mightee['RM_err'].values
+grm     = mightee['GRM_FS'].values      # use 1 deg median
+grm_err = mightee['GRM_FSerr'].values   # use 1 deg median
+grm1    = mightee['GRM1'].values        # use 1 deg median
+z       = mightee['best_z'].values
+
+rrm = rm - grm1
+
+std = np.std(rrm)
+rrm = rrm[np.where(np.abs(rrm)<=2*std)]
+z = z[np.where(np.abs(rrm)<=2*std)]
+
+bins, idx_bin, n_bin = bin_z(z, nbins=7)
+mu_bin = 0.5*np.diff(bins)+bins[:-1]
+
+#print("Number of sources per bin:",n_bin)
+#print("Central redshift per bin:",mu_bin)
+
+mu_rrm = [np.average(rrm[idx_bin == j]) for j in range(1, len(bins))]
+sig_rrm= [np.std(rrm[idx_bin == j]) for j in range(1, len(bins))]
+ste_rrm= sig_rrm/np.sqrt(n_bin)
+ste_sig = standard_error_on_stdev(sig_rrm, n_bin)
+
+#print("Means: {}".format(mu_rrm))
+#print("Standard errors:", ste_rrm)
+
+pl.rcParams["figure.figsize"] = (8,5)
+pl.scatter(z, rrm, s=1)
+pl.errorbar(mu_bin, mu_rrm, yerr=ste_rrm, xerr=0.5*np.diff(bins), c='red', ls='', capsize=3.)
+pl.xlabel('z')
+pl.ylabel('RRM [rad/m^2]')
+pl.ylim(-40,40)
+pl.savefig('./plots/mightee_rrm_z.png')
+pl.show()
+
+pl.scatter(z, np.abs(rrm), s=1)
+pl.errorbar(mu_bin, sig_rrm, yerr=ste_sig, xerr=0.5*np.diff(bins), c='red', ls='', capsize=3.)
+pl.xlabel('z')
+pl.ylabel('RRM rms [rad/m^2]')
+pl.ylim(0,40)
+pl.savefig('./plots/mightee_rrmsig_z.png')
+pl.show()
--- utils.py
+++ utils.py
@@ -1,23 +1,160 @@
 from astropy.io import fits
 from astropy.table import Table
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from astropy.utils.exceptions import AstropyWarning
 import pandas as pd
+import numpy as np
+import warnings
 
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
-def read_catalogue(catfile):
+def read_catalogue(catfile, field_centre):
 
-    #with fits.open(catfile) as data:
-    #    df = pd.DataFrame(data[0].data)
-    
+    warnings.filterwarnings('ignore', category=AstropyWarning)
     dat = Table.read(catfile, format='fits')
+
+    field = catfile.split('/')[-2]
+    dat.add_column(field,name='pointing')
+
+    coord = SkyCoord(dat['ra'],dat['dec'],unit="deg")
+    distr = coord.separation(field_centre).degree
+    dat.add_column(distr,name='distance')
+    
     df = dat.to_pandas()
     
     return df
 
+# -------------------------------------------------------------------------------
+# updated from Russ' version:
 
-def merge_cats(catlist):
+def merge_cats(catlist, distol=10.):
 
+    ncat = len(catlist)
+    nmatches = 0
+    for c1 in range(0,ncat-1):
+        for c2 in range(c1+1,ncat):
+            j=0
+            while True:
+                found,n,dra,ddec = a_in_b(catlist[c1].iloc[[j]],catlist[c2],distol)
+                if found:
+                    nmatches = nmatches + 1
+                    if catlist[c1].iloc[[j]]['distance'].values[0] < catlist[c2].iloc[[n]]['distance'].values[0]:
+                        catlist[c2] = catlist[c2].drop(index=n).reset_index(drop=True)
+                        j+=1
+                    else:
+                        catlist[c1] = catlist[c1].drop(index=j).reset_index(drop=True)
+                else:
+                    j+=1
+
+                if j>=len(catlist[c1]['ra'].values): break
+
+    print("Found {} matches".format(nmatches))
     df = pd.concat(catlist)
-        
+            
     return df
 
+# -------------------------------------------------------------------------------
+# from Russ:
+
+def a_in_b(a,b,distol):
+    found = False
+    dis = []; dras = []; ddecs = []
+    for i in range(len(b['ra'].values)):   
+        dd = 3600.0*(b['dec'].values[i] - a['dec'].values)
+        dr = 3600.0*(b['ra'].values[i] - a['ra'].values)*np.cos(a['dec'].values*np.pi/180.0)
+        dis.append(np.sqrt(dd*dd + dr*dr))
+        dras.append(dr)
+        ddecs.append(dd)
+    dis = np.array(dis)
+    n = np.argmin(dis)
+    if dis[n] < distol:
+        found = True
+    return found, n, dras[n], ddecs[n]
+
+# -------------------------------------------------------------------------------
+
+def filter_z(df, has_z=True, spec_z=True, zmax=2.):
+
+    if has_z:
+        df = df.loc[df['best_z'] != -99]
     
+    if spec_z:
+        df = df.loc[df['spec_z'] != -99]
+    
+    if zmax!=None:
+        df = df.loc[df['best_z'] < zmax]
+    
+    return df
+    
+# -------------------------------------------------------------------------------
+
+def filter_glat(df, bmin=25.):
+
+    df = df.loc[df['b'].abs() > bmin]
+
+    return df
+
+# -------------------------------------------------------------------------------
+
+def j2000_to_gal(ra, dec):
+
+    coord = SkyCoord(ra=ra*u.degree, dec=dec*u.degree, frame='fk5')
+    coord = coord.transform_to('galactic') 
+    
+    return coord.l.degree, coord.b.degree
+
+
+# -------------------------------------------------------------------------------
+
+def standard_error_on_stdev(std, n):
+
+    """
+    Function to return the standard error on the sample
+    standard deviation assuming a Normal distribution
+    https://math.stackexchange.com/questions/1259383/calculating-uncertainty-in-standard-deviation
+    https://stats.stackexchange.com/questions/156518/what-is-the-standard-error-of-the-sample-standard-deviation
+    """
+    
+    if hasattr(n, "__len__"):
+        std = np.array(std)
+        n   = np.array(n)
+    
+    se = std/np.sqrt(2*n)
+    
+    return se
+
+# -------------------------------------------------------------------------------
+
+def standard_error_on_variance(std, n):
+
+    """
+    Function to return the standard error on the sample
+    standard deviation assuming a Normal distribution
+    https://math.stackexchange.com/questions/1259383/calculating-uncertainty-in-standard-deviation
+    https://stats.stackexchange.com/questions/156518/what-is-the-standard-error-of-the-sample-standard-deviation
+    """
+    
+    if hasattr(n, "__len__"):
+        std = np.array(std)
+        n   = np.array(n)
+    
+    se = np.sqrt((2*std**4)/(n-1))
+    
+    return se
+
+# -------------------------------------------------------------------------------
+
+def bin_z(z, nbins=7):
+
+    d_z = 2./nbins
+    bins = np.arange(0,(nbins+1)*d_z,d_z)
+    mu_bin = 0.5*np.diff(bins)+bins[:-1]
+    idx_bin = np.digitize(z, bins)
+
+    n_bin = [len(z[idx_bin==j]) for j in range(1, len(bins))]
+
+    return bins, idx_bin, n_bin
+
+# -------------------------------------------------------------------------------
